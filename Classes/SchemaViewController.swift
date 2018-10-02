d465f29c1c43399fa41baabd913f9b484414b317
Mohamed <mohamed461994@gmail.com>
2018-10-02 17:07:07 +0200

    feat: new version test

---
+++ Classes/SchemaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SchemaViewController.swift
+//  JsonSchemaPod
+//
+//  Created by Muhamed Shaban on 10/2/18.
+//
+
+import UIKit
+
+class SchemaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
