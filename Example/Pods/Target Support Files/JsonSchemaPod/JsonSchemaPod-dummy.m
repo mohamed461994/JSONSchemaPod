0e24056ece330571b683c0b2f39efbc6ae38d684
Mohamed <mohamed461994@gmail.com>
2018-10-02 14:48:36 +0200

    intial commit

---
+++ Example/Pods/Target Support Files/JsonSchemaPod/JsonSchemaPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JsonSchemaPod : NSObject
+@end
+@implementation PodsDummy_JsonSchemaPod
+@end
---
+++ PodCode/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  JsonSchemaPod
+//
+//  Created by Muhamed Shaban on 10/2/18.
+//
+
+import Foundation
+
+public class TestClass {
+    public func hello() {
+        print("Hello World!!")
+    }
+}
